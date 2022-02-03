94d84bde7e84a1acd7cb0c794c182e51630cbe42
Paul Laberge <plaberge@outlook.com>
2022-02-03 15:01:46 -0500

    minor:  Scaffolding for using Azure App Configuration for connection strings implemented

--- nhlstatswebportal/nhlstatswebportal_blazor/Program.cs
+++ nhlstatswebportal/nhlstatswebportal_blazor/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using nhlstatswebportal_blazor.Data;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var configBuilder = new ConfigurationBuilder();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -10,6 +12,21 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
+
+configBuilder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("NHLSTATS_APPCONFIG_CONNECTIONSTRING"));
+
+var config = configBuilder.Build();
+
+//************************************************************************
+// Get configuration data from Azure App Configuration
+// Database:
+string keyName = "nhlstats:database:azure";
+string connectionString = config[keyName];
+
+// App Insights:
+keyName = "nhlstats:monitor:appinsights";
+string telemetryKey = config[keyName];
+//************************************************************************
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
